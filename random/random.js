410550fea0c6665d15b89f1fca32756aa742b68c
mukrram-bajwa <mukrram.rahman@gmail.com>
2018-01-26 09:19:54 +0100

    Initial Benchmarks for AWS lambda and Google cloud functions

---
+++ random/random.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ *
+ * Author: Mukrram Ur Rahman
+ *
+ * Description: In this simple example I am going to generate a random number based on
+ * twister pseudorandom number generator.
+ *
+ */
+
+var MersenneTwister = require('mersenne-twister');
+
+exports.rand = function(event, context, callback) {
+                
+                    var generator = new MersenneTwister();
+                    var max_rand = 0;
+                    for(var i = 0; i < 20000000; i++) {
+                        var tmp = generator.random_int31();
+                        if(tmp > max_rand)
+                            max_rand = tmp;
+                    }
+                    // This will be used for when we hook up API Gateway.
+                    context.done(null, { number :  tmp});
+};

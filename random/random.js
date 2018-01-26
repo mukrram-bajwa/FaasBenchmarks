007e848a415848bec8304b20b39957746c01afc3
mukrram-bajwa <mukrram.rahman@gmail.com>
2018-01-26 21:43:54 +0100

    changed random func for google cloud

--- random/random.js
+++ random/random.js
@@ -11,15 +11,14 @@
 
 var MersenneTwister = require('mersenne-twister');
 
-exports.rand = function(event, context, callback) {
-                
-                    var generator = new MersenneTwister();
-                    var max_rand = 0;
-                    for(var i = 1; i < 60000000; i++) {
-                        var tmp = generator.random_int31();
-                        if(tmp > max_rand)
-                            max_rand = tmp;
-                    }
-                    // This will be used for when we hook up API Gateway.
-                    callback(null, { number :  tmp});
+exports.rand = function rand (req, res) {
+	var generator = new MersenneTwister();
+    var max_rand = 0;
+    for(var i = 1; i < 60000000; i++) {
+    	var tmp = generator.random_int31();
+        if(tmp > max_rand)
+        	max_rand = tmp;
+    }
+    res.write({ number :  tmp});
+    res.end();
 };
